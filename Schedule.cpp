1cd97f56324b5f85c4d270657f88e6b2ff4fb35a
zach <agonist@yahoo.com>
2018-04-18 21:30:48 -0700

    Added basic structure for Work Schedule.

---
+++ Schedule.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using  namespace std;
+
+class TimeSegment {
+    unsigned int StartTime;
+    unsigned int EndTime;
+};
+
+class ContactInformation {
+    string PhoneNumber;
+    string Address;
+    string City;
+    string State;  
+    vector<TimeSegment> AvailableTime;
+};
+
+class Person {
+    string Name;
+    ContactInformation Information;
+};
+
+class Shifts {
+    unsigned int Time;
+    unsigned int Duration;
+    Person *Assigned;
+};
+
+class WorkSchedule {
+    vector<Shifts> Shifts;
+};
+
+int main() {
+    return 0;
+}
